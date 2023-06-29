ebcb7d48dff3057db28a0f3a84942ed00667cbec
BradGrensewich <bradgrensewich@gmail.com>
2023-06-29 13:52:45 +0700

    some mouse events and the event object

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const app = Vue.createApp({
             showText: true,
             title: 'Kafka on the Shore',
             author: 'Harakuri Murikama',
-            age: 61
+            age: 61,
+            x: 0,
+            y: 0
         }
     },
     //a method property of the compenent filled with methods to be used
@@ -23,6 +25,19 @@ const app = Vue.createApp({
         },
         toggleShowText() {
             this.showText = !this.showText
+        },
+        //whenever we have events in the browser like double click, mouseleave ect. we get access to an
+        //optional even object. it's denoted here as e. it must be the first parameter to access it
+        handleEvent(e, data) {
+            //events have a ton of properties. here's an example
+            console.log(e, e.type)
+            if (data) {
+                console.log(data)
+            }
+        },
+        handleMouseMove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
         }
     }
     //template: '<h2>I am the template</h2>'
