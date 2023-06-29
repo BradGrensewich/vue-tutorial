fe50d819c86734e8845941a5dcef45bfa78c4287
BradGrensewich <bradgrensewich@gmail.com>
2023-06-29 13:22:55 +0700

    added basic vue conditionals

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const app = Vue.createApp({
-    //data, fucntions
+    //create a function within the object and return an object
+    //properties inside the object are accesible inside the template
     data() {
         return {
+            showText: true,
             title: 'Kafka on the Shore',
             author: 'Harakuri Murikama',
             age: 61
         }
     },
+    //a method property of the compenent filled with methods to be used
     methods: {
+        //it doesn't have to be this complicated i just wanted to pass an argument in
         changeTitle(title) {
             if (this.title === 'Kafka on the Shore') {
                 this.title = 'Dance Dance Dance'
@@ -16,6 +20,9 @@ const app = Vue.createApp({
             } else {
                 this.title = 'Kafka on the Shore'
             }
+        },
+        toggleShowText() {
+            this.showText = !this.showText
         }
     }
     //template: '<h2>I am the template</h2>'
